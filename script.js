89f18941bacd8a446eb470853e0685c837f9a8dd
LyubchenkoVadim <lubcenko039@gmail.com>
2018-02-16 20:58:40 +0200

    op

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+   $('.btn-slide') .click(function () {
+  //     $('#panel').slideToggle('slow');
+   //    $('.btn-slide').toggleClass('active');
+       $('#panel').animate({height:'300px', width: '100%'},3000);
+   });
+
+$('.delete').click(function () {
+    $(this).parent().animate({height: '20px'},'slow');
+    $(this).parent().animate({width: '20px'},'slow')
+});
+$('.js-ajax').click(function () {
+    event.preventDefault();
+    $.get($(this).attr('href'), function (data) {
+        $('#content').html(data);
+    });
+    //alert('Кнопка нажата');
+
+}) ;
+
+//$('.js-ajax').on('click', function () {
+ //   $('.js-ajax').off('click', function () {
+//})
+
+});
